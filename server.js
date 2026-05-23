f302142af06bb5af543a63be91b23ba4f0f0d37b
Dugiii <dugidemeg@gmail.com>
2026-05-22 23:04:24 -0500

    refactor server.js to include movie data and organize request handlers

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const http = require('http');
+const getReq = require('./methods/getRequest');
+const postReq = require('./methods/postRequest');
+const putReq = require('./methods/putRequest');
+const deleteReq = require('./methods/getRequest');
+// const { getReq, postReq, putReq, deleteReq } = require('./methods');
+
+let movies = require('./data/movies.json');
+
 // require('dotenv').config();
 
 const port = process.env.PORT || 8001;
 
 const server = http.createServer((req, res) => {
+    req.movies = movies;
+    
     switch (req.method) {
         case "GET":
             getReq(req, res);
