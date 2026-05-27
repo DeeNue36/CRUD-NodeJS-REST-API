e00ace57f7fc9620ca159e2795bbeaa1ca412d8d
Dugiii <dugidemeg@gmail.com>
2026-05-26 23:52:32 -0500

    add UUID validation to getRequest.js for improved error handling

--- methods/getRequest.js
+++ methods/getRequest.js
@@ -1,14 +1,19 @@
 module.exports = (req, res) => {
     let baseUrl = req.url.substring(0, req.url.lastIndexOf('/') + 1);
     console.log(baseUrl);
+
     let id = req.url.split('/');
-    console.log(id);
+    const regexV4 = new RegExp(/^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i);
 
     if (req.url === '/api/movies') {
         res.statusCode = 200;
         res.setHeader("Content-Type", "application/json");
         res.write(JSON.stringify(req.movies));
         res.end();
+    }
+    else if (!regexV4.test()) {
+        res.writeHead(400, {"Content-Type": "application/json"});
+        res.end(JSON.stringify({"title": "Bad Request","message": "UUID Not Found!!"}));
     }
     else {
         res.writeHead(404, {"Content-Type": "application/json"});
