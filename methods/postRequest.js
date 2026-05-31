a7631f69dd5ca74de16a7b5b4bab8e47aea24413
Dugiii <dugidemeg@gmail.com>
2026-05-31 01:06:19 -0500

    refactor postRequest.js to streamline response handling and error messaging

--- methods/postRequest.js
+++ methods/postRequest.js
@@ -5,9 +5,14 @@ module.exports = async (req, res) => {
     if (req.url === '/api/movies') {
         try {
             let body = await bodyParser(req);
-            
+            body.id = cryptoUUID.randomUUID();
+            req.movies.push(body);
+            res.writeHead(201, {"Content-Type": "application/json"});
+            res.end();
         } catch (err) {
             console.log(err);
+            res.writeHead(400, {"Content-Type": "application/json"});
+            res.end(JSON.stringify({"title": "Bad Request","message": "UUID Not Found!!"}));
         }
         
     }
