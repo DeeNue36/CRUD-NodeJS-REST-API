37b76312ec0404a57c0ae43959547b00317ee688
Dugiii <dugidemeg@gmail.com>
2026-05-31 22:56:12 -0500

    fix writeToFile.js to correct file write parameters

--- utils/writeToFile.js
+++ utils/writeToFile.js
@@ -5,7 +5,8 @@ const path = require('path');
 module.exports = (data) => {
     try {
         fs.writeFileSync(
-            path.join(__dirname, '..', 'data', 'movies.json', JSON.stringify(data), 'utf-8')
+            path.join(__dirname, '..', 'data', 'movies.json'), 
+            JSON.stringify(data), 'utf-8'
         );
         
     } catch (error) {
