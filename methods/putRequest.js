0fadcb99b2904dc3b055635628242dcea8f9ee4b
Dugiii <dugidemeg@gmail.com>
2026-06-04 00:55:17 -0500

    feat: refactor putRequest.js to use async/await for improved handling of movie updates

--- methods/putRequest.js
+++ methods/putRequest.js
@@ -1,7 +1,7 @@
 const bodyParser = require('../utils/bodyParser');
 const writeToFile = require('../utils/writeToFile');
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
     let baseUrl = req.url.substring(0, req.url.lastIndexOf('/') + 1);
     let id = req.url.split('/')[3];
     const regexV4 = new RegExp(/^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i);
@@ -9,5 +9,32 @@ module.exports = (req, res) => {
     if (!regexV4.test(id)) {
         res.writeHead(400, {"Content-Type": "application/json"});
         res.end(JSON.stringify({"title": "Bad Request","message": "UUID Not Found!!"}));
+    }
+    else if (baseUrl === '/api/movies/' && regexV4.test(id)) {
+        try {
+            let body = await bodyParser(req);
+            const movieIndex = req.movies.findIndex((movie) => {
+                return movie.id === id;
+            });
+            if (movieIndex === -1) {
+                res.statusCode = 404;
+                res.write(JSON.stringify({"title": "Not Found","message": "Movie Not Found!!"}));
+                res.end();
+            }
+            else {
+                req.movies[movieIndex] = { id, ...body };
+                writeToFile(req.movies);
+                res.writeHead(200, {"Content-Type": "application/json"});
+                res.end(JSON.stringify(req.movies[movieIndex]));
+            }
+        } catch (error) {
+            console.log(error);
+            res.writeHead(400, {"Content-Type": "application/json"});
+            res.end(JSON.stringify({"title": "Bad Request","message": "UUID Not Found!!"}));
+        }
+    }
+    else {
+        res.writeHead(404, {"Content-Type": "application/json"});
+        res.end(JSON.stringify({"title": "Not Found","message": "Route Not Found!!"}));
     }
 };
